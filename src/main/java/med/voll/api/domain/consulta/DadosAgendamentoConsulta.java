5f0faaec92250e281c944dbc3bd4f38cb0ca81c2
Luan Valente <luanbvalente@hotmail.com>
2023-10-31 22:48:44 -0300

    Agendamento de Consultas

--- src/main/java/med/voll/api/domain/consulta/DadosAgendamentoConsulta.java
+++ src/main/java/med/voll/api/domain/consulta/DadosAgendamentoConsulta.java
@@ -2,17 +2,20 @@ package med.voll.api.domain.consulta;
 
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotNull;
+import med.voll.api.domain.medico.Especialidade;
 
 import java.time.LocalDateTime;
 
 public record DadosAgendamentoConsulta(
 
-        Long id,
+        Long idMedico,
 
         @NotNull
         Long idPaciente,
 
         @NotNull
         @Future
-        LocalDateTime data) {
+        LocalDateTime data,
+
+        Especialidade especialidade) {
 }
